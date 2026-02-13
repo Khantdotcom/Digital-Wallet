fa4f892adb4e304a25b2cad5099013c86dff0773
Khantdotcom <kokhanthtay@gmail.com>
2026-02-13 22:47:50 +0700

    feat(backend): add secure test

---
+++ backend/src/main/java/com/khant/wallet/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.khant.wallet.repository;
+
+import com.khant.wallet.domain.User;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+  Optional<User> findByEmail(String email);
+}
